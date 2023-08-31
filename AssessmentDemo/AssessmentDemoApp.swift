a9a97e5c9ab7bb8b4cb1c9ebacd84a7d50af6aa6
Amit Saini <post2amitsaini@gmail.com>
2023-08-31 19:30:05 +0530

    Initial Commit

---
+++ AssessmentDemo/AssessmentDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssessmentDemoApp.swift
+//  AssessmentDemo
+//
+//  Created by Amit Saini on 31/08/23.
+//
+
+import SwiftUI
+
+@main
+struct AssessmentDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
